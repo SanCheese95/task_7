333981a7b6e5bf402eadfd15bf67fcea797eb86a
Sancheese95 <sasha_pavlyukovec@mail.ru>
2024-10-16 22:30:43 +0300

    Initial commit

--- task7/task1.py
+++ task7/task1.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-def count_unique_characters(string):
-    lower_string = string.lower()
-    unique_chars = list(filter(lambda c: lower_string.count(c.lower()) == 1, lower_string))
-    print(unique_chars)
-    return len(unique_chars)
-
-message = input('Введите предложение: ')
-unique_count = count_unique_characters(message)
-print("Количество уникальных символов в строке:", unique_count)
-=======
 from functools import reduce
 
 floats = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
